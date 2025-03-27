71420dae9c73d74ef0c4d0b803588b2ba9f035f3
ericamady <ermadisonyouman@mail.fhsu.edu>
2025-03-26 21:08:35 -0700

    Update read_single.php

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -25,7 +25,7 @@ if(isset($_GET['author_id'])){
 }
 
 else{
-   $quotes->author_id = null;
+   $quotes->author_id = '';
 }
 
 if(isset($_GET['category_id'])){
@@ -34,7 +34,7 @@ if(isset($_GET['category_id'])){
 }
 
 else{
-   $quotes->category_id = null;
+   $quotes->category_id = '';
 }
 
 
